7c1ce0cf3579fc6b1936e52221839718b3171518
miljab <jablonskimilosz1337@gmail.com>
2024-12-26 21:10:22 +0100

    Add sign up functionality

---
+++ controllers/authController.js
@@ -0,0 +1,53 @@
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const { prisma } = require("../prisma/client");
+
+const registerValidation = [
+  body("username")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters long")
+    .custom(async (value) => {
+      const user = await prisma.user.findUnique({ where: { username: value } });
+      if (user) {
+        throw new Error("Username already exists");
+      }
+      return true;
+    }),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords do not match");
+    }
+    return true;
+  }),
+];
+
+registerPost = [
+  registerValidation,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("signup", {
+        errors: errors.array(),
+      });
+    }
+
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      await prisma.user.create({
+        data: {
+          username,
+          password: hashedPassword,
+        },
+      });
+      res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Error registering user");
+    }
+  },
+];
